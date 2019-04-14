97f10f65cce96f90a46bbe31213a63e59fccc577
gabriel-plata <muricy.maisev@gmail.com>
2019-04-14 13:03:31 -0300

    Arrumado menu

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    function exibeIem(e){
-        $(this).find('conteudo-1').show();
-        alert("teste");
-    }
-
-    $('#item-1-conteudo').on('click', exibeItem);
-
-});
