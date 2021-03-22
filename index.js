2ec298a65f9258df39231136ca8e5049ced8f79b
Gabriel Plata <39375326+gabriel-plata@users.noreply.github.com>
2021-03-21 22:10:16 -0300

    regex
    refatoração no javascript com regex
    refatoração html
    refatoração css
    alguns imagens modificadas

--- index.js
+++ index.js
@@ -2,63 +2,29 @@ let conteudo = '#home';
 let itemConteudo = "#home-menu-opcao-1";
 const show = {"display" : "block"};
 const hide = {"display" : "none"};
-const shadowTextItemHome = $('.home-item-menu-text').css("text-shadow");
-const shadowBoxItemHome = $('.home-item-menu-text').css("box-shadow");
-const shadowBoxItemHomeHover = $("#shadows-item-home-hover").css("box-shadow");
-const shadowTextItemHomeHover = $("#shadows-item-home-hover").css("text-shadow");
-const shadowBoxItemMenu = $('.area-item-menu').css("box-shadow");
-const shadowBoxItemMenuHover = $("#shadows-item-menu-hover").css("box-shadow");
-const bgColorItemMenu = $('.area-item-menu').css("background-color");
-const bgColorItemMenuHover = $("#shadows-item-menu-hover").css("background-color");
-
 
 handlersMenu(conteudo,itemConteudo);
 
-
 function handlersMenu(conteudo,itemConteudo){
-
-    $('.home-item-menu-text').on("mouseover", function(){
-        $(this).css({"text-shadow" : shadowTextItemHomeHover});
-        $(this).css({"box-shadow" : shadowBoxItemHomeHover});
-    });
-    
-    $('.home-item-menu-text').on("mouseleave", function(){
-        $(this).css({"text-shadow" : shadowTextItemHome});
-        $(this).css({"box-shadow" : shadowBoxItemHome});
-    });
-    
-    $('.area-item-menu').on("mouseover", function(){
-        $(this).css({"background-color": bgColorItemMenuHover});
-        $(this).css({"box-shadow" : shadowBoxItemMenuHover});
-    });
-    
-    $('.area-item-menu').on("mouseleave", function(){
-        $(this).css({"background-color": bgColorItemMenu});
-        $(this).css({"box-shadow" : shadowBoxItemMenu});
-    });
-    
+  
     $('.area-item-menu').on("click", function(){
-        let idClicado = $(this).attr("id");
-        if(idClicado == "opcao-menu-1" && conteudo != "#home"){
+        let idClicado = "#" + $(this).attr("id").replace(/(\w+)-(\w+)-(\w+)/, '$3');
+        if(idClicado != conteudo){
             $(conteudo).css(hide);
-            $("#home").css(show);
-            conteudo = "#home";
-        }else if(idClicado == "opcao-menu-2" && conteudo != "#projetos"){
-            $(conteudo).css(hide);
-            $("#projetos").css(show);
-            conteudo = "#projetos";
-        }else if(idClicado == "opcao-menu-3" && conteudo != "#certificados"){
-            $(conteudo).css(hide);
-            $("#certificados").css(show);
-            conteudo = "#certificados";
-        }else if(idClicado == "opcao-menu-4" && conteudo != "#curriculo"){
-            $(conteudo).css(hide);
-            $("#curriculo").css(show);
-            conteudo = "#curriculo";
-        }else if(idClicado == "opcao-menu-5" && conteudo != "#contato"){
-            $(conteudo).css(hide);
-            $("#contato").css(show);
-            conteudo = "#contato";
+            $(idClicado).css(show);
+            conteudo = idClicado;
+        }
+    });
+
+    $('.home-item-menu-text').on("click", function(){
+        let idClicado = "#" + $(this).closest('div').attr("id");
+        if(idClicado != itemConteudo){
+            let regex = /(\W)(\w+)-(\w+)-(\w+)-(\w+)/;
+            let anterior = itemConteudo.replace(regex, '$1$2-conteudo-$4-$5');
+            let novo =  anterior.replace(regex, '$1$2-$3-$4-' + idClicado.replace(regex, '$5'));
+            $(anterior).css(hide);
+            $(novo).css(show);
+            itemConteudo = idClicado;
         }
     });
 }
